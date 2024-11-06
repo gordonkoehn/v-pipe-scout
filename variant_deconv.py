d128c8367714243f6a850516293ca277dd8a1dd9
Gordon J. Köhn <gordon@koehn.net>
2024-11-06 15:21:46 +0100

    add lollipoop run

--- variant_deconv.py
+++ variant_deconv.py
@@ -14,17 +14,72 @@ def app():
 
     st.title('Generate Plot')
 
-    x_values = st.text_input('Enter X values (comma-separated)', '1,2,3,4,5')
-    y_values = st.text_input('Enter Y values (comma-separated)', '1,4,9,16,25')
+    import requests
+    from PIL import Image
+    from io import BytesIO
+    import base64
 
-    if st.button('Generate Plot'):
-        x = list(map(int, x_values.split(',')))
-        y = list(map(int, y_values.split(',')))
+    # Prebuilt YAML configurations
+    yaml_option_1 = """
+    '2024-01-01':
+    - BA.4
+    - BA.5
+    - BA.2.75
+    - BA.2.86
+    - BQ.1.1
+    - XBB.1.5
+    - XBB.1.9
+    - XBB.1.16
+    - XBB.2.3
+    - EG.5
+    - JN.1
+    - BA.2.87.1
+    - BA.1
+    - BA.2
+    - KP.2
+    - KP.3
+    """
 
-        response = requests.post('http://68.221.168.92:8000/plot', json={'x': x, 'y': y})
+    yaml_option_2 = """
+    '2024-01-01':
+    - BA.4
+    - BA.5
+    - BA.2.75
+    - BA.2.86
+    - BQ.1.1
+    - XBB.1.5
+    - XBB.1.9
+    - XBB.1.16
+    - XBB.2.3
+    - EG.5
+    - JN.1
+    - BA.2.87.1
+    - BA.1
+    - BA.2
+    - KP.2
+    - KP.3
+    - XEC
+    """
+
+    st.title('Generate Plot from YAML')
+
+    # Dropdown to select prebuilt YAML configurations
+    yaml_options = {
+        'No XEC': yaml_option_1,
+        'With XEC 2': yaml_option_2,
+        'Custom': ''
+    }
+
+    selected_option = st.selectbox('Variant YAML configuration', list(yaml_options.keys()))
+
+    # Text area to input or display YAML data
+    yaml_data = st.text_area('Enter YAML data', value=yaml_options[selected_option])
+
+    if st.button('Run Lollipop'):
+        response = requests.post('http://<your-container-ip>:8000/run_lollipop', json={'yaml': yaml_data})
         if response.status_code == 200:
             plot_url = response.json()['plot_url']
             image = Image.open(BytesIO(base64.b64decode(plot_url.split(',')[1])))
             st.image(image)
         else:
-            st.error('Failed to generate plot')
+            st.error('Failed to execute Lollipop command')
