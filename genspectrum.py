6a7670865de4ff05230f47897160f14233240f36
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-01-24 17:31:33 +0100

    trying to connect to localhost

--- genspectrum.py
+++ genspectrum.py
@@ -27,12 +27,53 @@ def app():
                 lapisDateField='date'
                 />
             </gs-app>
+
+            </head>
+            <body>
+            <!-- Component documentation: https://genspectrum.github.io/dashboard-components/?path=/docs/visualization-mutations-over-time--docs -->
+            <gs-app lapis="http://localhost:8080/2">
+                <gs-mutations-over-time
+                lapisFilter='{"sampling_dateFrom":"2024-08-23", "sampling_dateTo": "2024-10-23"}'
+                sequenceType='amino acid'
+                views='["grid"]'
+                width='100%'
+                height='100%'
+                granularity='week'
+                lapisDateField='sampling_date'
+                />
+            </gs-app>
             </body>
         </html>
     """,
         height=600,
     )
-
+''' 
+    components.html(
+        """
+        <html>
+        <head>
+        <script type="module" src="https://unpkg.com/@genspectrum/dashboard-components@latest/standalone-bundle/dashboard-components.js"></script>
+        <link rel="stylesheet" href="https://unpkg.com/@genspectrum/dashboard-components@latest/dist/style.css" />
+        </head>
+            <body>
+            <!-- Component documentation: https://genspectrum.github.io/dashboard-components/?path=/docs/visualization-mutations-over-time--docs -->
+            <gs-app lapis="http://localhost:8080/2">
+                <gs-mutations-over-time
+                lapisFilter='{"sampling_dateFrom":"2024-08-23", "sampling_dateTo": "2024-10-23",}'
+                sequenceType='amino acid'
+                views='["grid"]'
+                width='100%'
+                height='100%'
+                granularity='week'
+                lapisDateField='date'
+                />
+            </gs-app>
+            </body>
+        </html>
+    """,
+        height=600,
+    )
+ '''
 
     ## Allow for user input by Gene
 
