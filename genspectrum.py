2da9aed2018804b1940b268b7463c6a0aa9ff720
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-01-24 15:53:46 +0100

    adding mutations over time

--- genspectrum.py
+++ genspectrum.py
@@ -10,26 +10,25 @@ def app():
     components.html(
         """
         <html>
-    <head>
-    <script type="module" src="https://unpkg.com/@genspectrum/dashboard-components@latest/standalone-bundle/dashboard-components.js"></script>
-    <link rel="stylesheet" href="https://unpkg.com/@genspectrum/dashboard-components@latest/dist/style.css" />
-    </head>
-
-    <body>
-    <!-- Component documentation: https://genspectrum.github.io/dashboard-components/?path=/docs/visualization-mutations-over-time--docs -->
-    <gs-app lapis="https://lapis.cov-spectrum.org/open/v2">
-        <gs-mutations-over-time
-        lapisFilter='{"region":"Europe","country":"Switzerland","dateFrom":"2024-01-23","nextcladePangoLineage":"JN.1*"}'
-        sequenceType='amino acid'
-        views='["grid"]'
-        width='100%'
-        height='100%'
-        granularity='week'
-        lapisDateField='date'
-        />
-    </gs-app>
-    </body>
-    </html>
+        <head>
+        <script type="module" src="https://unpkg.com/@genspectrum/dashboard-components@latest/standalone-bundle/dashboard-components.js"></script>
+        <link rel="stylesheet" href="https://unpkg.com/@genspectrum/dashboard-components@latest/dist/style.css" />
+        </head>
+            <body>
+            <!-- Component documentation: https://genspectrum.github.io/dashboard-components/?path=/docs/visualization-mutations-over-time--docs -->
+            <gs-app lapis="https://lapis.cov-spectrum.org/open/v2">
+                <gs-mutations-over-time
+                lapisFilter='{"region":"Europe","country":"Switzerland","dateFrom":"2024-01-23","nextcladePangoLineage":"JN.1*"}'
+                sequenceType='amino acid'
+                views='["grid"]'
+                width='100%'
+                height='100%'
+                granularity='week'
+                lapisDateField='date'
+                />
+            </gs-app>
+            </body>
+        </html>
     """,
         height=600,
     )
