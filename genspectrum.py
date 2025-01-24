bff84765b7a2410a87458c1a4d6ce456c85e65b5
Gordon J. Köhn <gordon.koehn@dbsse.ethz.ch>
2025-01-24 18:04:25 +0100

    query for proportions

--- genspectrum.py
+++ genspectrum.py
@@ -1,9 +1,45 @@
 import streamlit as st
-
 import streamlit.components.v1 as components
+import aiohttp
+import asyncio
+import logging
+import yaml
 
+import pandas as pd
 
 LAPIS_URL = "http://localhost:8080/"
+
+
+# Load configuration from config.yaml
+with open('config.yaml', 'r') as file:
+    config = yaml.safe_load(file)
+
+server_ip = config.get('server', {}).get('lapis_address', 'http://default_ip:8000')
+
+async def fetch_data():
+    url = 'http://localhost:8080/sample/aminoAcidMutations'
+    params = {
+        'sampling_dateFrom': '2024-08-23',
+        'sampling_dateTo': '2024-10-23',
+        'minProportion': 0.5,
+        'orderBy': 'proportion',
+        'limit': 100,
+        'downloadAsFile': 'false'
+    }
+    headers = {
+        'accept': 'application/json'
+    }
+
+    async with aiohttp.ClientSession() as session:
+        async with session.get(url, params=params, headers=headers) as response:
+            if response.status == 200:
+                data = await response.json()
+                return data
+            else:
+                logging.error(f"Failed to fetch data. Status code: {response.status}")
+
+# To run the async function
+# asyncio.run(fetch_data())
 
 def app():
     # bootstrap 4 collapse example
@@ -27,10 +63,7 @@ def app():
                 lapisDateField='date'
                 />
             </gs-app>
-
-            </head>
-            <body>
-            <!-- Component documentation: https://genspectrum.github.io/dashboard-components/?path=/docs/visualization-mutations-over-time--docs -->
+                        <!-- Component documentation: https://genspectrum.github.io/dashboard-components/?path=/docs/visualization-mutations-over-time--docs -->
             <gs-app lapis="http://localhost:8080/2">
                 <gs-mutations-over-time
                 lapisFilter='{"sampling_dateFrom":"2024-08-23", "sampling_dateTo": "2024-10-23"}'
@@ -42,38 +75,14 @@ def app():
                 lapisDateField='sampling_date'
                 />
             </gs-app>
-            </body>
-        </html>
-    """,
-        height=600,
-    )
-''' 
-    components.html(
-        """
-        <html>
-        <head>
-        <script type="module" src="https://unpkg.com/@genspectrum/dashboard-components@latest/standalone-bundle/dashboard-components.js"></script>
-        <link rel="stylesheet" href="https://unpkg.com/@genspectrum/dashboard-components@latest/dist/style.css" />
-        </head>
+
+            </head>
             <body>
-            <!-- Component documentation: https://genspectrum.github.io/dashboard-components/?path=/docs/visualization-mutations-over-time--docs -->
-            <gs-app lapis="http://localhost:8080/2">
-                <gs-mutations-over-time
-                lapisFilter='{"sampling_dateFrom":"2024-08-23", "sampling_dateTo": "2024-10-23",}'
-                sequenceType='amino acid'
-                views='["grid"]'
-                width='100%'
-                height='100%'
-                granularity='week'
-                lapisDateField='date'
-                />
-            </gs-app>
             </body>
         </html>
     """,
         height=600,
     )
- '''
 
     ## Allow for user input by Gene
 
@@ -86,9 +95,20 @@ def app():
     ### Make Query for list of mutations wiht Such proportions
     ### Ensure this list is not to large to be displayed
 
-    ### For each mutation in the list get the counts of the mutation over time
+    date_range = [pd.to_datetime("2024-09-30"), pd.to_datetime("2024-10-16")]
+    gene = ["ORF1a"]
+    proportions = [0.05, 0.5]
+    sequence_type = "amino acid"
 
-    ### Display the counts of the mutation over time
+    data = asyncio.run(fetch_data())
+
+    # Display the data
+    st.write(data)
+
+
+### For each mutation in the list get the counts of the mutation over time
+
+### Display the counts of the mutation over time
 
 
 
